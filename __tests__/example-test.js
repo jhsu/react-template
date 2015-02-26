c1e50196d2fae6b7c7929f5e32389465ef1f8144
Joe <jhsu.x1@gmail.com>
2015-02-25 17:14:21 -0800

    update deps

--- __tests__/example-test.js
+++
@@ -1,8 +0,0 @@
-describe('Example test', function() {
-  var React = require('react/addons');
-  var TestUtils = React.addons.TestUtils;
-
-  it('adds 1 + 2 to equal 3', function() {
-    expect(1 + 2).toBe(3);
-  });
-});
